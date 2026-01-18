164b0e2f6dfbe2b8ff3f5975cb169bc4c7029269
leeknivoc <leeknivoc@gmail.com>
2026-01-18 03:48:59 +0000

    api주소변경 및 구매기능추가 및 반응형 웹 구현

--- src/product/index.js
+++ src/product/index.js
@@ -2,31 +2,46 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constant.js";
+import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   useEffect(function () {
+    getProduct();
+  }, []);
+
+  const getProduct = () => {
     axios
-      .get(
-        `https://af6652e4-ff02-4a5b-9929-2a79fa4b39d6.mock.pstmn.io/products1/${id}`
-      )
+      .get(`${API_URL}/products/${id}`)
       .then(function (result) {
-        setProduct(result.data);
+        setProduct(result.data.product);
         console.log("성공", result);
       })
       .catch(function (error) {
         console.error("에러발생 :", error);
       });
-  }, []);
-
+  };
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었숩니다");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가발생했습니다, ${error.message}`);
+      });
+  };
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다...</h1>;
   } else {
     return (
       <div>
         <div id="image-box">
-          <img src={"/" + product.imageUrl} />
+          <img src={`${API_URL}/${product.imageUrl}`} />
         </div>
         <div id="profile-box">
           <img src="/images/icons/avatar.png" />
@@ -35,7 +50,19 @@ function ProductPage() {
         <div id="contents-box">
           <div id="name">{product.name}</div>
           <div id="price">{product.price}원</div>
-          <div id="createdAt">2020년 12월 8일</div>
+          <div id="createdAt">
+            {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+          </div>
+          <Button
+            id="purchase-button"
+            size="large"
+            type="primary"
+            danger
+            onClick={onClickPurchase}
+            disabled={product.soldout === 1 ? true : false} 
+          >
+            재빨리 구매하기
+          </Button>
           <div id="description">{product.description}</div>
         </div>
       </div>
--- src/upload/index.js
+++ src/upload/index.js
@@ -1,5 +1,127 @@
+import { Button, Form, InputNumber, Upload, message } from "antd";
+import { Divider, Input } from "antd";
+import "./index.css";
+import { useState } from "react";
+import { API_URL } from "../config/constant";
+import axios from "axios";
+import {useHistory} from "react-router-dom";
 function UploadPage() {
-  return <h1>업로드화면</h1>;
+  const [imageUrl, setImageUrl] = useState(null);
+  const history = useHistory();
+  const onSubmit = (values) => {
+    console.log(values);
+    axios
+      .post(`${API_URL}/products`, {
+        name: values.name,
+        description: values.description,
+        price: parseInt(values.price),
+        seller: values.seller,
+        imageUrl: imageUrl,
+      })
+      .then((result) => {
+        console.log("성공", result);
+        history.replace("/");
+      }).catch((error) => {
+        console.error("에러발생", error);
+        message.error(`에러가 발생했습니다. ${error.message}`)
+      });
+  };
+  const onChangeImage = (info) => {
+    if (info.file.status === "uploading") {
+      return;
+    }
+    if (info.file.status === "done") {
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
+  };
+  return (
+    <div id="upload-container">
+      <Form name="상품업로드" onFinish={onSubmit}>
+        <Form.Item
+          name="upload"
+          label={<div className="upload-label">상품 사진</div>}
+        >
+          <Upload
+            name="image"
+            action={`${API_URL}/image`}
+            listType="picture"
+            showUploadList={false}
+            onChange={onChangeImage}
+          >
+            {imageUrl ? (
+              <img id="upload-image" src={`${API_URL}/${imageUrl}`} />
+            ) : (
+              <div id="upload-img-placeholder">
+                <img src="/images/icons/camera.png"></img>
+                <span>이미지를 업로드해주세요.</span>
+              </div>
+            )}
+          </Upload>
+        </Form.Item>
+
+        <Divider />
+
+        <Form.Item
+          name="seller"
+          label={<div className="upload-label">판매자 명</div>}
+          rules={[{ required: true, message: "판매자이름을 입력해주세요" }]}
+        >
+          <Input
+            className="upload-name"
+            size="large"
+            placeholder="이름을 입력해주세요"
+          />
+        </Form.Item>
+
+        <Divider />
+
+        <Form.Item
+          name="name"
+          label={<div className="upload-label">상품이름</div>}
+          rules={[{ required: true, message: "상품이름을 입력해주세요" }]}
+        >
+          <Input
+            className="upload-name"
+            size="large"
+            placeholder="상품이름을 입력해주세요"
+          />
+        </Form.Item>
+
+        <Divider />
+
+        <Form.Item
+          name="price"
+          label={<div className="upload-label">상품가격</div>}
+          rules={[{ required: true, message: "상품가격을 입력해주세요" }]}
+        >
+          <InputNumber defaultValue={0} className="upload-price" size="large" />
+        </Form.Item>
+
+        <Divider />
+
+        <Form.Item
+          name="description"
+          label={<div className="upload-label">상품 소개</div>}
+          rules={[{ required: true, message: "상품소개를 입력해주세요" }]}
+        >
+          <Input.TextArea
+            size="large"
+            id="product-description"
+            showCount={true}
+            maxLength={300}
+            placeholder="상품소개를 적어주세요"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button id="submit-button" size="large" htmlType="submit">
+            문제 등록하기
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadPage;
